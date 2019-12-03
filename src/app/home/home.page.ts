c643f11085ec465bb648c032f5511693d09b86d2
anil <dollorinfotech@gmail.com>
2019-12-03 13:37:34 +0530

    JQuery Intregration

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit } from '@angular/core';
+import * as $ from 'jquery';
+ 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
 
-  constructor() {}
+	constructor() {
+	  
+	}
+	
+	ngOnInit() {
+		$(document).ready(function(){
+			//alert(‘JQuery is working!!’);
+			console.log('Everting Is working...');
+			//alert('JQuery Working....');
+		});
+	}
 
 }
